00e7b76233f5991337d3539d5dd5b41d842796a5
Yuhei <ynoko1024@gmail.com>
2019-04-14 23:56:12 +0900

    プロジェクト ファイルを追加します。

---
+++ Joblvchk/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Joblvchk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+            ///進捗チェックアプリケーションの制作
+            ///　
+            ///機能リスト
+            ///① 時刻と進捗状況を確認するための残り時間を表示する
+            ///② 確認ウィンドウには2つ程度の候補を表示し、選択状況に応じて音声の再生,カウントの終了等を行う
+            ///③ 確認ウィンドならびにメインウィンドウには音声再生用のボタンを配置
+            ///④ 最前面表示とミュートモード（吹き出しのような機能）を実装したい
+            ///⑤ 表示する画像等は選択・読み込みができる機能も付けたい。
+
+        }
+    }
+}
